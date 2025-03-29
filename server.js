5bbb1500d0bd9959ce6ddc7c9837bd482775b7f4
ABDOULLAH ALALGUI <g135808232@um5.ac.ma>
2025-03-29 20:56:33 +0000

    Update server.js

--- server.js
+++ server.js
@@ -15,9 +15,20 @@ app.use(express.json());
 
 // Google Drive Authentication
 const auth = new google.auth.GoogleAuth({
-  keyFile: "credentials.json",
-  scopes: ["https://www.googleapis.com/auth/drive"],
+    credentials: {
+        type: "service_account",
+        project_id: process.env.GOOGLE_PROJECT_ID,
+        private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+        client_email: process.env.GOOGLE_CLIENT_EMAIL,
+        client_id: process.env.GOOGLE_CLIENT_ID,
+        auth_uri: process.env.GOOGLE_AUTH_URI,
+        token_uri: process.env.GOOGLE_TOKEN_URI,
+        auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_CERT_URL,
+        client_x509_cert_url: process.env.GOOGLE_CLIENT_CERT_URL,
+    },
+    scopes: ["https://www.googleapis.com/auth/drive"],
 });
+
 const drive = google.drive({ version: "v3", auth });
 
 // Use memory storage instead of disk storage
